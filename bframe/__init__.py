459c3fa2751c0a43af8f1cefe717e65df8d9a11d
luojie <1342104001@qq.com>
2023-07-13 14:42:31 +0800

    [update] 0.0.12 release 1. 修复对wsgi的异常处理 2. 支持自定义状态码 3. 修复query params的异常匹配 4. 日志默认仅控制台输出

--- bframe/__init__.py
+++ bframe/__init__.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__version__ = "0.0.11"
+__version__ = "0.0.12"
 
 
 __all__ = ["request", "g", "current_app", "Frame", "Redirect", "Logger", "abort", "WSGIProxy"]
