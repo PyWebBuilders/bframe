973196ce07270ebce7cd0b1067883f8bb2052c4e
luojie <1342104001@qq.com>
2023-10-12 17:01:56 +0800

    [bug] fix testcase find project bug

--- tests/frame_test.py
+++ tests/frame_test.py
@@ -3,7 +3,7 @@ import sys
 import unittest
 import json
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
 
 from _frame_demo import app
 
